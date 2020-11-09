1a70cbd6c4e95f97806c741622056535ad794827
Brent <bshafer93@gmail.com>
2020-11-09 13:45:10 -0800

    Another Commit
    I got a blue background with centered text.

--- LetsTravelApp/App.js
+++ LetsTravelApp/App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React , {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+
+
+class SplashScreen extends Component {
+  render() {
+    return (
+      <View style={styles.titleScreen}>
+        <Text>Let's</Text>
+        <Text>Travel!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SplashScreen />
   );
 }
 
@@ -18,4 +33,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  titleScreen:{
+    flex:1,
+    backgroundColor: '#34526D',
+    alignItems: 'center',
+    fontSize: 50,
+    fontWeight: "bold",
+    justifyContent: 'center',
+  },
+
+
 });
+
+
